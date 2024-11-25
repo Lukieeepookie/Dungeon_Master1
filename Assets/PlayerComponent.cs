b9318ad193beb65b24950b9a41f5b3f7c24d8c27
Lukieeepookie <157555034+Lukieeepookie@users.noreply.github.com>
2024-11-25 00:40:17 -0800

    Improved Player Component
    Made it better.

--- Assets/PlayerComponent.cs
+++ Assets/PlayerComponent.cs
@@ -8,12 +8,16 @@ public class PlayerComponent : MonoBehaviour
     #region Public Variables
 
     public float maxSpeed = 10f;
-    public float AccRate = 10f;
-    public float DecelRate = 15f;
+    public float AccRate = 100f;
+    public float DecelRate = 100f;
+    public float airAcceleration = 50f;
     public float jumpLeniency = 0.1f;
-    public float jumpForce = 10f;
-    public int jumpCount = 2;
-    public float gravityApex = 0.2f;
+    public float jumpForce = 16f;
+    public float minJumpTime = 0.25f;
+    public float airTimeLeniency = 1f;
+    public float airTimeGravity = 0.5f;
+    public float airTimeBoost = 2f;
+    public float CoyoteTime = 0.1f;
 
     public Vector3 respawn;
 
@@ -26,19 +30,21 @@ public class PlayerComponent : MonoBehaviour
     private Rigidbody2D rb;
     private GravityComponent gravity;
 
-    private int jumps = 0;
-
     private float boundaryY = -10;
     private bool isFacingRight = true;
-    private float movementX;
-    private float moveDirection = 0;
+    private float movementX = 0f;
     private float appliedVelocity = 0f;
+    private float coyoteTimer = 0f;
+
     private float jumpTimer = 0f;
+    private float jumpingTimer = 0f;
+
+    private bool jumpTry = false;
     private bool jumping = false;
 
-    private bool lightGravity = false;
-
-    private float gravityTimer = 0f;
+    private bool heavyGravity = false;
+    private bool airTime = false;
+    private bool grounded = false;
 
     #endregion
     void Start()
@@ -48,69 +54,179 @@ public class PlayerComponent : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        this.transform.rotation = Quaternion.identity;
-        if (this.transform.position.y < boundaryY)
+        if (coyoteTimer > CoyoteTime)
         {
-            this.transform.position = respawn;
-            this.rb.velocity = Vector2.zero;
-        }
-        movementX = rb.velocity.x;
-        #region Inputs
-        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
-        {
-            moveDirection = 1f;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
-        {
-            moveDirection = -1f;
+            grounded = false;
         }
         else
         {
-            moveDirection = 0f;
+            coyoteTimer += Time.deltaTime;
         }
+        if (IsGrounded())
+        {
+            grounded = true;
+            coyoteTimer = 0f;
+        }
+        this.transform.rotation = Quaternion.identity; //keep player rotation
+        if (this.transform.position.y < boundaryY) //reset player position
+        {
+            this.transform.position = respawn;
+            this.rb.velocity = new Vector2(0, 0);
+        }
+        Flip();
+        jump();
+        gravity.gravityMult = (heavyGravity ? 1.5f : 1f);
+        if (airTime)
+        {
+            gravity.gravityMult = airTimeGravity;
+        }
+    }
+    void FixedUpdate()
+    {
+        movement();
+    }
+
+    void Flip()
+    {
+        bool pastFacingRight = isFacingRight;
+        if (movementX > 0f)
+        {
+            isFacingRight = true;
+        }
+        else if (movementX < 0f)
+        {
+            isFacingRight = false;
+        }
+        if (pastFacingRight != isFacingRight)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    void jump()
+    {
+        print("Jumping : " + jumping);
+        bool jumped = false;
         if (jumpTimer > jumpLeniency)
         {
-            jumping = false;
+            jumpTry = false;
         }
         else
         {
             jumpTimer += Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.Z) && jumps > 0)
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            jumps -= 1;
-            jumpTimer = 0;
+            jumpTimer = 0f;
+            jumpTry = true;
+        }
+        if (jumpTry && grounded)
+        {
             jumping = true;
+            jumped = true;
         }
-
-        #endregion
-        Flip();
-        #region Left & Right Movement
-        appliedVelocity = movementX + (moveDirection * AccRate * Time.deltaTime);
-        if (appliedVelocity > maxSpeed)
+        if (jumping)
         {
-            appliedVelocity = maxSpeed;
+            if (jumped)
+            {
+                jumpingTimer = 0f;
+                jumped = false;
+                rb.velocity = new Vector2(this.rb.velocity.x, jumpForce);
+            }
+            if (!Input.GetKey(KeyCode.Z) && jumpingTimer > minJumpTime || this.rb.velocity.y < 0f)
+            {
+                if (this.rb.velocity.y > 0.5f)
+                {
+                    this.rb.velocity = new Vector2(this.rb.velocity.x,0.5f);
+                }
+                heavyGravity = true;
+            }
+            if (this.rb.velocity.y > -airTimeLeniency && this.rb.velocity.y < airTimeLeniency)
+            {
+                airTime = true;
+            }
+            else
+            {
+                airTime = false;
+            }
+            if (IsGrounded() && this.rb.velocity.y <= 0f)
+            {
+                airTime = false;
+                jumping = false;
+                heavyGravity = false;
+            }
+            jumpingTimer += Time.deltaTime;
         }
-        else if (appliedVelocity < -maxSpeed)
+    }
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
         {
-            appliedVelocity = -maxSpeed;
+            if (movementX < 0f)
+            {
+                appliedVelocity = 0f;
+            }
+            else
+            {
+                if (IsGrounded())
+                {
+                    appliedVelocity = movementX + (AccRate * Time.deltaTime);
+                }
+                else
+                {
+                    if (airTime)
+                    {
+                        appliedVelocity = movementX + (airAcceleration * Time.deltaTime * airTimeBoost);
+                    }
+                    else
+                    {
+                        appliedVelocity = movementX + (airAcceleration * Time.deltaTime);
+                    }
+                }
+            }
+            if (appliedVelocity > maxSpeed)
+            {
+                appliedVelocity = maxSpeed;
+            }
         }
-        if (moveDirection == 1f && movementX < 0f)
+        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
         {
-            appliedVelocity = 0f;
+            if (movementX > 0f)
+            {
+                appliedVelocity = 0f;
+            }
+            else
+            {
+                if (IsGrounded())
+                {
+                    appliedVelocity = movementX - (AccRate * Time.deltaTime);
+                }
+                else
+                {
+                    if (airTime)
+                    {
+                        appliedVelocity = movementX - (airAcceleration * Time.deltaTime * airTimeBoost);
+                    }
+                    else
+                    {
+                        appliedVelocity = movementX - (airAcceleration * Time.deltaTime);
+                    }
+                }
+            }
+            if (appliedVelocity < -maxSpeed)
+            {
+                appliedVelocity = -maxSpeed;
+            }
         }
-        else if (moveDirection == -1f && movementX > 0f)
-        {
-            appliedVelocity = 0f;
-        }
-        if (moveDirection == 0)
+        else
         {
             if (movementX > 0f)
             {
                 appliedVelocity = movementX - (DecelRate * Time.deltaTime);
-                if (appliedVelocity <= 0f)
+                if (appliedVelocity < 0f)
                 {
                     appliedVelocity = 0f;
                 }
@@ -118,49 +234,15 @@ public class PlayerComponent : MonoBehaviour
             else if (movementX < 0f)
             {
                 appliedVelocity = movementX + (DecelRate * Time.deltaTime);
-                if (appliedVelocity >= 0f)
+                if (appliedVelocity > 0f)
                 {
                     appliedVelocity = 0f;
                 }
             }
         }
-
         this.rb.velocity = new Vector2(appliedVelocity, this.rb.velocity.y);
-        #endregion
-
-        #region Jump Movement
-        if (IsGrounded() && !jumping)
-        {
-            jumps = jumpCount;
-        }
-        if (jumping && IsGrounded())
-        {
-            jumping = false;
-            rb.velocity = new Vector2(this.rb.velocity.x, jumpForce);
-            lightGravity = true;
-        }
-        if (Input.GetKeyUp(KeyCode.Z) || !IsGrounded() && this.rb.velocity.y <=0)
-        {
-            lightGravity = false;
-        }
-
-        gravity.setGravityMultiplier(lightGravity ? 0.5f : 1f);
-
-        #endregion
+        movementX = this.rb.velocity.x;
     }
-
-    void Flip()
-    {
-        if (isFacingRight && moveDirection < 0f || !isFacingRight && moveDirection > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            print(isFacingRight);
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-    }
-    
     bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundedCheck.position, 0.2f, groundLayer);
